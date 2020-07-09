f63c8b00cd31ca4a2205860f73b1f816d0170524
imhilla <hillaryodhiambo282@gmail.com>
2020-07-09 14:00:34 +0300

    add create action and update new action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,21 @@ class PostsController < ApplicationController
 	before_action :authenticate_user!, only: %i[new create]
 
 	def new
-		@post = Post.new
+		@post = current_user.posts.build
 	end
 
+	def create
+		@post = current_user.posts.build(post_params)
+		if @post.save
+			redirect_to new_post_path, notice: 'Post was succesfully created.'
+		else 
+			render 'new'
+		end
+	end
 
+	private
+	def post_params 
+		params.require(:post).permit(:body)
+	end
 
 end
