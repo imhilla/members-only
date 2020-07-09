b896bb9c8db43fa087ffcb3e2c28db011a8a5e8b
imhilla <hillaryodhiambo282@gmail.com>
2020-07-09 18:21:41 +0300

    run rubocop

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,26 @@
 class PostsController < ApplicationController
-	before_action :authenticate_user!, only: %i[new create]
+  before_action :authenticate_user!, only: %i[new create]
 
-	def new
-		@post = current_user.posts.build
-	end
+  def new
+    @post = current_user.posts.build
+  end
 
-	def create
-		@post = current_user.posts.build(post_params)
-		if @post.save
-			redirect_to root_path, notice: 'Post was succesfully created.'
-		else 
-			render 'new'
-		end
-	end
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to root_path, notice: 'Post was succesfully created.'
+    else
+      render 'new'
+    end
+  end
 
-	def index
-		@posts = Post.all
-	end
+  def index
+    @posts = Post.all
+  end
 
-	private
-	def post_params 
-		params.require(:post).permit(:body)
-	end
+  private
 
+  def post_params
+    params.require(:post).permit(:body)
+  end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-  	p 
+    p
     params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 
