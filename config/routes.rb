ce878fbecd91659c7f90f1e9a45f9785f7cbb1b2
ozovalihasan <ozovalihasan@gmail.com>
2020-07-14 14:03:48 +0300

    Update paths of sign_up links with created form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_scope :user do
-    get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+    get "/sign_up" => "signup#new", as: "new_user_registration" # custom path to sign_up/registration
   end
   devise_for :users, :controllers => {:registrations => "registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
