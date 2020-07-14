0a1f3e106240c743ee5afa7668959142d2c0056a
imhilla <hillaryodhiambo282@gmail.com>
2020-07-14 13:04:53 +0300

    add signup form, create signup controller, add helper methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+  end
   devise_for :users, :controllers => {:registrations => "registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, only: [:new, :create, :index]
+  resources :signup, only: [:new, :create]
   root 'posts#index'
 end
