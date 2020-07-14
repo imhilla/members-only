ff42d1fbe6a63bbc69853bfbdf2a8f5454230bab
ozovalihasan <ozovalihasan@gmail.com>
2020-07-14 13:36:22 +0300

    Add user_sign_in helper to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
 
+  helper_method :user_singin?
 	private
 
 	def user_authenticate_changed
     redirect_to root_path, notice: "You must login" unless user_signed_in?
   end
 
+  def user_singin?
+    !current_user1.nil?
+  end
 end
