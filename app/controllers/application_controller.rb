6486121c70e32d4e7b36d24ce4665d1cb23f2319
ozovalihasan <ozovalihasan@gmail.com>
2020-07-14 13:48:39 +0300

    Update index.html.erb with user_signin? helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
 class ApplicationController < ActionController::Base
 
-  helper_method :user_singin?
+  helper_method :user_signin?
+
 	private
 
 	def user_authenticate_changed
     redirect_to root_path, notice: "You must login" unless user_signed_in?
   end
 
-  def user_singin?
-    !current_user1.nil?
+  def user_signin?
+    !current_user.nil?
   end
 end
