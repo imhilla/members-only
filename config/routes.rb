b72f5cc3c728f8af5407c8696919612b035c8c7d
ozovalihasan <ozovalihasan@gmail.com>
2020-07-15 10:55:21 +0300

    Remove unnecessary codes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,5 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, only: [:new, :create, :index]
-  resources :signup, only: [:new, :create]
   root 'posts#index'
 end
